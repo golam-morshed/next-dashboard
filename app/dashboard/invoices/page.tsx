ff4a411f9c63fdeb0d0feb9b4b40bfc9975af60e
Golam Morshed <golamdotmorshed@gmail.com>
2025-09-23 20:30:16 +0600

    Integrate debounced search functionality in the search component, enhance pagination logic to dynamically create page URLs, and fetch total pages for the invoices page to improve navigation and user experience.

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,15 +6,19 @@ import Table from '@/app/ui/invoices/table'
 import { CreateInvoice } from '@/app/ui/invoices/buttons'
 import { lusitana } from '@/app/ui/fonts'
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons'
+import { fetchInvoicesPages } from '@/app/lib/data'
 
 async function Page(props: {
 searchParams?: Promise<{
   query?: string;
   page?: string
 }>}){
-const queryParams = await props?.searchParams
+  const queryParams = await props?.searchParams
+  console.log(queryParams)
 const query = queryParams?.query || ''
 const currentPage = Number(queryParams?.page) || 1
+const totalPages = await fetchInvoicesPages(query)
+console.log(totalPages)
 
   return (
     <div className="w-full">
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,20 +2,22 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce'
 
 export default function Search({ placeholder }: { placeholder: string }) {
-  const router = useRouter()
-  const pathName = usePathname()
-  const searchParams = useSearchParams() 
-  const handleSearch = (term: string) => {
-    const params = new URLSearchParams(searchParams)
+  const router = useRouter();
+  const pathName = usePathname();
+  const searchParams = useSearchParams();
+  const handleSearch = useDebouncedCallback((term: string) => {
+    const params = new URLSearchParams(searchParams);
+    params.set('page', '1')
     if(term){
       params.set('query', term)
     } else {
       params.delete('query')
     }
     router.replace(`${pathName}?${params.toString()}`)
-  }
+  }, 300)
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
